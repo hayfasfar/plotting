c3ac757efb41cda68f7867d95cabdb90b812af24
hayfasfar <hsfar@lx03.hep.ph.ic.ac.uk>
2020-04-08 11:57:52 +0100

    plotter code

--- plotter.py
+++ plotter.py
@@ -31,7 +31,7 @@ categories = {}
 #categories["mu1mu2jet_DY_2tights_CR_"] = "(ntightMuons == 2)*(dimuon_mass > 85.)*(nselectedJets>=2)"
 
 #categories["mu1mu2jet_Wjets_CR_"] = "(ntightMuons == 1)*(nlooseMuons == 1)*(dimuon_mass > 110. or ( dimuon_mass < 80. and dimuon_mass > 15.))*(Jet_Muon_MET_Mu_mT >=60.)*(MET_pt < 100.)*(nselectedJets<6)*(Jet_Muon_ht <=180.)*(Jet_Muon_minPhi >1.)*(lepJet_deltaR>=1.)"
-categories["mu1mu2jet_TT_CR_"] ="(ntightMuon == 1)*(nlooseMuons == 1)*(MET_pt > 100)"
+categories["mu1mu2jet_TT_CR_"] ="(ntightMuon == 1)*(nlooseMuons == 1)*(nselectedJets>=8)"
 
 #categories["mu1mu2jet_QCD_CR_"] = "(ntightMuons == 1)*(nlooseMuons == 1) * (dimuon_mass < 10.) *(Jet_Muon_minPhi<0.8) * (MET_pt < 50.) " 
 
@@ -56,23 +56,29 @@ class Variable:
 	self.jesDown = jesDown
 	self.jerUp = jerUp
 	self.jerDown = jerDown 
-	self.sysUnc = True
+	self.sysUnc = sysUnc
     def Add(self, hist, title, isSignal=False, isData=False):
-        hist.SetDirectory(0)
+        hist[-1].SetDirectory(0)
         if isSignal:
-            self.signals.append(hist)
+            self.signals.append(hist[-1])
             hist.SetLineStyle(len(self.signals))
-            self.leg.AddEntry(hist, title, "l")
+            self.leg.AddEntry(hist[-1], title, "l")
         elif isData:
-            self.data = hist
-            self.leg.AddEntry(hist, title, "p")
+            self.data = hist[-1]
+            self.leg.AddEntry(hist[-1], title, "p")
         else:
-            self.stack.Add(hist)
-            self.sumMC.Add(hist)
-            self.leg.AddEntry(hist, title, "f")
+	    
+	    self.stack.Add(hist[-1])
+            self.sumMC.Add(hist[-1])
+            self.leg.AddEntry(hist[-1], title, "f")
+
+	    if self.sysUnc  :
+        	hist[-3].SetDirectory(0)
+		self.sumUncUp.Add(hist[-3])
+        	hist[-2].SetDirectory(0)
+		self.sumUncDown.Add(hist[-2])
 	    
     def Draw(self, suffix, opt):
-        print ("plotting "+self.varexp)
         canvas = makeCanvas(name=self.varexp)
         upperPad = ROOT.TPad("upperPad", "upperPad", 0, 0.33, 1, 1)
         lowerPad = ROOT.TPad("lowerPad", "lowerPad", 0, 0, 1, 0.33)
@@ -88,6 +94,8 @@ class Variable:
         lowerPad.Draw()
         upperPad.cd()
 
+	## Draw MC histograms which are stacked together in self.stack. 
+
         self.stack.Draw(opt)
         self.stack.SetMinimum(1)
         if self.logy:
@@ -95,27 +103,70 @@ class Variable:
             upperPad.SetLogy()
         else:
             self.stack.SetMaximum(self.stack.GetMaximum()*1.6)
+
+	## Draw Signal if it is signal MC comparison study. otherwise the array self.signals will be empty.
         for signal in self.signals:
             signal.SetFillStyle(0)
             signal.Draw("HIST SAME")
+
+	### now draw data if it is CR Data study. 
         self.data.Draw("P SAME")
         self.hist_ratio = self.data.Clone("ratio histogram")
-        self.hist_ratio.Divide(self.sumMC) 
-        lowerPad.cd()
-        self.hist_ratio.SetMinimum(0.5)
-        self.hist_ratio.SetMaximum(1.5)
+        self.hist_ratio.Divide(self.sumMC)
+	## add SysUnc here
+ 
+	## Clone data first. you need to divide data by observable SystUp. 
+	#self.histSysUp_ratio = self.data.Clone("ratio histogram")
+	#self.histSysUpClone = self.sumUncUp.Clone("copy up")
+	#self.histSysUp_ratio.Divide(self.sumUncUp)
+	self.histSysUp_ratio = self.sumUncUp.Clone("ratio plot")
+	self.histSysUp_ratio.Divide(self.sumMC) 
+	
+	self.histSysDown_ratio = self.sumUncDown.Clone("ratio plot")	
+	self.histSysDown_ratio.Divide(self.sumMC)
+	#self.histSysDown_ratio = self.data.Clone()
+	#self.histSysDown_ratio.Divide(self.sumUncDown)
+	lowerPad.cd()
+        self.hist_ratio.SetMinimum(0.8)
+        self.hist_ratio.SetMaximum(1.2)
         self.hist_ratio.GetXaxis().SetTitle(self.name)
         self.hist_ratio.GetXaxis().SetTitleOffset(2.5)
         self.hist_ratio.Draw("P")
-
+	self.histSysUp_ratio.SetFillStyle(0)
+	self.histSysUp_ratio.SetLineColor(4)
+	#self.histSysUp_ratio.SetFillColorAlpha(ROOT.kBlue, 0.35)
+	self.histSysUp_ratio.Draw("HIST SAME")
+	self.histSysDown_ratio.SetFillStyle(0)
+	self.histSysDown_ratio.SetLineColor(2)
+	#self.histSysDown_ratio.SetFillColorAlpha(ROOT.kBlue, 0.35)
+	self.histSysDown_ratio.Draw("HIST SAME")
+	rootObj = []
         line = ROOT.TLine(self.xmin, 1, self.xmax, 1)
         line.Draw("SAME")
+	for ibin in range(self.sumMC.GetNbinsX()):
+            c = self.sumMC.GetBinCenter(ibin+1)
+            w = self.sumMC.GetBinWidth(ibin+1)
+            m = self.sumMC.GetBinContent(ibin+1)
+            if m > 0.0:
+                h = min(self.sumMC.GetBinError(ibin+1)/m, 0.399)
+                box = ROOT.TBox(c-0.5*w, 1-h, c+0.5*w, 1+h)
+                box.SetFillStyle(3345)
+                box.SetLineColor(ROOT.kGray+1)
+                box.SetFillColor(ROOT.kGray)
+                rootObj.append(box)
+                box.Draw("SameF")
+                box2 = ROOT.TBox(c-0.5*w, 1-h, c+0.5*w, 1+h)
+                box2.SetFillStyle(0)
+                box2.SetLineColor(ROOT.kGray+1)
+                box2.SetFillColor(ROOT.kGray)
+                rootObj.append(box2)
+                box2.Draw("SameL")
         canvas.cd()
         self.leg.Draw("SAME")
         makeCMSText(0.13, 0.97,additionalText="Simulation Preliminary")
-        makeText(0.13, 0.78, 0.4, 0.78, "jets pt > 30. GeV")
+        makeText(0.13, 0.78, 0.4, 0.78, "nselectedJets>= 8")
         makeLumiText(0.7, 0.97)
-        canvas.SaveAs("plots/"+suffix+self.varexp+".pdf")
+        canvas.SaveAs("plots/"+suffix+self.varexp+"nselectedJets8.pdf")
 
 # This class prepares a given sample by scaling to int. luminosity
 class Sample:
@@ -166,8 +217,6 @@ class Process:
 			histjesDown = rdf.Histo1D(args, jesDown , weight)
 			histjerUp = rdf.Histo1D(args, jerUp , weight)
 			histjerDown = rdf.Histo1D(args, jerDown , weight)
-			histTotUp = rdf.Histo1D(args,varexp, weight)
-			histTotDown =rdf.Histo1D(args,varexp, weight)
             else:
                 #tmp_hist = rdf.Histo1D(args, varexp, weight)
 		tmp_hist = rdf.Define("var" , varexp).Histo1D(args, "var", weight)
@@ -181,30 +230,41 @@ class Process:
 			histjesDown.Add(tmp_hist.GetValue())
 	    		histjerUp.Add(tmp_hist.GetValue())
 			histjerDown.Add(tmp_hist.GetValue())
+	if sysUnc : 
+	        histTotUp = hist.Clone() 
+		histTotDown = hist.Clone() 			
+		for i in range(1,hist.GetNbinsX()+1) :
+			bin_nominal = hist.GetBinContent(i) 
+			bin_jesUp = histjesUp.GetBinContent(i) 
+			bin_jesDown = histjesDown.GetBinContent(i)
+			bin_jerUp = histjerUp.GetBinContent(i) 
+			bin_jerDown = histjerDown.GetBinContent(i)
+
 			
-	for i in range(1,hist.GetNbinsX()+1) : 
-		alpha = hist.GetBinContent(i) 
-		beta = histjesUp.GetBinContent(i) 
-		betaDown = histjesDown.GetBinContent(i)
-		gamma = histjerUp.GetBinContent(i) 
-		gammaDown = histjerDown.GetBinContent(i)
-		epsilon1 = beta - alpha 
-		epsilon2 = gamma - alpha 
-		systTotalUp = math.sqrt(epsilon1*epsilon1 + epsilon2*epsilon2)	
-		histTotUp.SetBinContent(i , alpha + systTotalUp)
-		epsilon1 = betaDown - alpha
-                epsilon2 = gammaDown - alpha
-		systTotalDown = math.sqrt(epsilon1*epsilon1 + epsilon2*epsilon2)
-                histTotDown.SetBinContent(i , alpha + systTotalDown)
+
+			eps_jesUp = bin_jesUp - bin_nominal
+			eps_jesDown = bin_jesDown - bin_nominal  
+			eps_jerUp =  bin_jerUp - bin_nominal
+                	eps_jerDown = bin_jerDown - bin_nominal
+			epJesAvr = (eps_jesUp + eps_jesDown)/2.
+			epJerAvr = (eps_jesDown + eps_jerDown)/2.
+
+			sysTotalAvr =  math.sqrt(epJesAvr*epJesAvr + eps_jesDown*eps_jesDown)
+			histTotUp.SetBinContent(i , bin_nominal  + sysTotalAvr)
+                	histTotDown.SetBinContent(i , bin_nominal  - sysTotalAvr)
 
 		
+	if sysUnc :
+		histTotUp.SetLineWidth(2)
+		histTotDown.SetLineWidth(2) 
+		self.hists.append(histTotUp.Clone())
+		self.hists.append(histTotDown.Clone())
         hist.GetXaxis().SetTitle(args[1])
         hist.SetLineColor(ROOT.TColor.GetColor(colorscale(self.color, 0.8)))
         hist.SetFillColor(ROOT.TColor.GetColor(colorscale(self.color, 1)))
         hist.SetLineWidth(2)
         self.hists.append(hist.Clone())
-#	print "histo1D type is : ", type(self.hists[-1])
-        return self.hists[-1]
+	return self.hists
 
 # self.hists return a TH1F histogram nselectedJets.
 
@@ -255,6 +315,7 @@ qcd.add(qcd_15to20, qcd_20to30, qcd_30to50, qcd_50to80, qcd_80to120, qcd_120to17
 data = Process("data", "data", "#000000")
 data.add(runb, runc, rund, rune, runf, rung, runh)
 processes = [ wjets, qcd , tt , dyjets, data]
+#processes = [ qcd , tt , data]
 
 # variable is 1 line in the variable.yaml file. 
 # suffix is for example "mu1mu2jet_TT_CR_" 
